f6f0a3127319e5f24e1ce68034b9c15714c0043f
Chetna Pant <chetnapant91@gmail.com>
2025-09-04 22:38:02 +0530

    final commit

---
+++ Basic Projects/23-random-user/app.js
@@ -0,0 +1,35 @@
+const url = "https://randomuser.me/api/";
+import get from './utility/getElement.js'
+import getUser from './utility/getUser.js';
+
+const img = get(".user-img");
+const title = get(".user-title");
+const value = get(".user-value");
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')]
+
+
+const displayUser=(person)=>{
+  img.src=person.image;
+  value.textContent = person.name;
+  title.textContent=`My name is`;
+  btns.forEach((btn)=> btn.classList.remove('active'));
+  btns[0].classList.add('active')  ;
+  btns.forEach((btn)=>
+  {
+    const label =btn.dataset.label;
+    btn.addEventListener('click',()=>{
+       value.textContent = person[label];
+       title.textContent = `My ${label} is`;
+       btns.forEach((btn)=>btn.classList.remove('active'));
+       btn.classList.add('active');
+    })
+  })
+}
+
+const showUser =async()=>{
+  const person =await getUser();
+  displayUser(person);
+}
+window.addEventListener('DOMContentLoaded',showUser);
+btn.addEventListener('click',showUser);
